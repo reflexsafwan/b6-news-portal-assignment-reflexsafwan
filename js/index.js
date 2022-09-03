7e5aab2865a3de8ac04e2c8c2f171b4c192cb97e
reflexsafwan <reflex.safwan@gmail.com>
2022-09-03 14:25:21 +0600

    show news section added

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const loadCatagories = () => {
 
 
 const displayCatagories = (catagories) => {
+    // console.log(catagories)
     const categoriesContainer = document.getElementById('catagory-container');
     catagories.forEach((catagory) => {
         // console.log(catagory)
@@ -15,14 +16,52 @@ const displayCatagories = (catagories) => {
         ul.classList.add('d-flex')
 
         ul.innerHTML = `
-              <li> ${catagory.category_name}</li>
+            <a onclick= "loadingNews('${catagory.category_id}')" href = "#">
+                 <li> ${catagory.category_name}</li>
+           </a>
               `
         categoriesContainer.appendChild(ul)
     });
+}
 
-
-
-
+const loadingNews = (catagoryID) => {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${catagoryID}`
+    fetch(url)
+        .then(res => res.json())
+        .then(catagory => displayNews(catagory.data));
 
 }
+const displayNews = (catagory) => {
+    const showCard = document.getElementById('card-conatiner');
+    showCard.textContent = ""
+    const cardNumber = document.getElementById('post-number');
+    cardNumber.innerText = catagory.length;
+    catagory.forEach(card => {
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('row');
+        cardBody.classList.add('g-0');
+        cardBody.innerHTML = `
+                <div class="col-md-4">
+                <img src="${card.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">${card.title}</h5>
+                <p class="card-text"> ${card.details}</p >
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div >
+            </div >
+    `
+
+
+        showCard.appendChild(cardBody)
+    });
+
+}
+
+
+
+
+
 loadCatagories()
